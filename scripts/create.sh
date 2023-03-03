d993edda4b59b2140f1b19be77df77b9859ccafb
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2023-03-03 08:25:26 +0100

    Corrected yaml files errors

--- scripts/create.sh
+++ scripts/create.sh
@@ -2,6 +2,6 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
- --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
---region=us-east-1
---profile udacity1
+--region=us-east-1 \
+--profile udacity1 \
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
